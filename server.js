9ab4a5b3cd8979082eafe532ecf0a54f2a70ee7d
udikabudi <udizohar84@gmail.com>
2013-12-05 13:35:39 -0500

    db

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 //create db - mongo db
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI);
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+
 
  var kittySchema2 = mongoose.Schema({ name: String });
  var Kitten = mongoose.model('Kitten3', kittySchema2);
